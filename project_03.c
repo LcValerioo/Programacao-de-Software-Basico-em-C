8483bb09378bf0802b24c3be8e9fed184e66720a
Lucas Valério <126265243+LcValerioo@users.noreply.github.com>
2026-01-04 04:24:31 +0000

    implementando vetores e matrizes dinamicamente

---
+++ project_03.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//Implementando um vetor
+/*
+int main(){
+    int numeros[10];
+    for(int i; i < 10; i++){
+            numeros[i] = i;
+            printf("%d\n", numeros[i]);
+    }
+    return 0;
+}
+*/
+
+
+//Implementando uma tabela
+/*
+int main() {
+    int tabela[4][5];
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 5; j++){
+            tabela[i][j] = i * j;
+            printf("%d", tabela[i][j]);
+        }
+        printf("\n");
+    }
+    return 0; 
+}
+*/
+
+//Implementando um array "dinamicamente"
+/*
+int main(){
+    int *numeros = (int *) malloc(10 * sizeof(int));
+    if(numeros == NULL){
+        printf("Erro ao alocar memoria\n");
+        return 1;
+    }
+    for(int i = 0; i < 10; i++){
+        numeros[i] = i;
+        printf("Numero: %d\n", i);
+    }
+    free(numeros);
+    return 0;
+}
+*/
+
+//Implementando um array dinamicamente
+int main() {
+    int *array;
+    int size, i;
+
+    printf("Quantos números você deseja inserir? ");
+    scanf("%d", &size);
+
+    // Alocação dinâmica de memória
+    array = (int*) malloc(size * sizeof(int));
+
+    // Verificação de alocação bem-sucedida
+    if (array == NULL) {
+        printf("Erro de alocação de memória.\n");
+        return -1; // Encerra o programa com erro
+    }
+
+    // Inserção de números no array
+    for (i = 0; i < size; i++) {
+        printf("Digite o número %d: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    // Exibição dos números
+    printf("Você inseriu: ");
+    for (i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+
+    // Liberação da memória alocada
+    free(array);
+    
+    return 0;
+}
